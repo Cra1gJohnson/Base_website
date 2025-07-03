00d1904a1f90d8aeca0256b3aa212621818bb53b
Cra1gJohnson <cjohns65@terpmail.umd.edu>
2025-07-03 11:46:29 -0400

    spelling correction

--- src/assets/components/NavBar.jsx
+++ src/assets/components/NavBar.jsx
@@ -5,7 +5,7 @@ const NavBar = () => {
   return (
     <div id="nav">
       <h3>Home</h3>
-      <h3>Work Experince</h3>
+      <h3>Work Experience</h3>
       <h3>Projects</h3>
       <h3>About</h3>
     </div>
