50f245da339072edcaa73acb43581a849c6c9fbe
Cra1gJohnson <cjohns65@terpmail.umd.edu>
2025-07-07 00:02:03 -0400

    working on about page

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 // variable root that is used to render the single page that all jsx components will be rendered in
 // client vs 
-import './index.css'
-import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import NotFoundPage from './pages/NotFoundPage.jsx'
-import ResumeRender from './pages/ResumeRender.jsx'
-import ProjectExperience from './pages/ProjectExperience.jsx'
+import './index.css';
+import App from './App.jsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import NotFoundPage from './pages/NotFoundPage.jsx';
+import ResumeRender from './pages/ResumeRender.jsx';
+import ProjectExperience from './pages/ProjectExperience.jsx';
+import About from './pages/About.jsx';
 
 const router = createBrowserRouter([
   {path: '/', element: <App />},
   {path: '/resume', element: <ResumeRender />},
   {path: '/projects', element: <ProjectExperience />},
-  {path: '*', element: <NotFoundPage />}
+  {path: '*', element: <NotFoundPage />},
+  {path: '/about', element: <About />}
 ]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router} />
   </StrictMode>,
-)
+);
