15e55c497b502f13c9414641b1e4810423b5c19c
cwalker6 <walkercd21@gmail.com>
2025-07-09 01:10:17 -0400

    New Experience Page

---
+++ src/pages/Experience.jsx
@@ -0,0 +1,12 @@
+
+import './Experience.css';
+
+export default function Experience() {
+    return (
+        <div className="background">
+            <div className="main-body">
+                <h1>Project Experience</h1>
+            </div>
+        </div>
+    )
+}
