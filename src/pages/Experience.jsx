36bcad0128518ef78db597a2783aa952067de6fe
cwalker6 <walkercd21@gmail.com>
2025-07-09 11:44:55 -0400

    Thoughts on tab's in experience page and change of color scheme

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,10 +1,17 @@
 
 import './Experience.css';
 
+
+// I am going about this all wrong.
+// Tabs needs to be a seperate component that is imported into the Experience page.
+// That way it can be used in other pages and have the same format.
 export default function Experience() {
     return (
         <div className="background">
             <div className="main-body">
+                <div className="tabs-bar">
+                    <h1>test</h1>
+                </div>
                 <h1>Project Experience</h1>
             </div>
         </div>
